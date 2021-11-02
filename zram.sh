d1a0c7eef606b602529288e34e964a352d105ef3
Cesare Vigliani <cesarevigliani89@gmail.com>
2021-11-02 16:18:25 +0100

    Fix zram shell individual script
    fix: remove all sudo onto script, it will run as root user to allow all writes on configs files
    fix: correct bash syntax all over the script
    fix: correct sed syntax
    feature: add root check.

--- zram.sh
+++ zram.sh
@@ -1,31 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # this script is used to configure zram
+# launch thi script as root and as $ sudo ./zram.sh to not encour in array issue
 
-sudo apt install -y zram-config util-linux
-totalmem=`LC_ALL=C free | grep -e "^Mem:" | sed -e 's/^Mem: *//' -e 's/  *.*//'`
+if [ "$(whoami)" != 'root' ]; then
+        echo "Must be root to run $0"
+        exit 1;
+fi
+
+apt install -y zram-config util-linux
+totalmem=$(LC_ALL=C free | grep -e "^Mem:" | sed -e 's/^Mem: *//' -e 's/  *.*//')
 
 #if ram is less than 2 GB blocks will be of 256 MB
-if [ $totalmem -lt 3500000 ] ; 
+if [ "$totalmem" -lt 3500000 ] ; 
 then 
     MEMORY_BLOCK=512
-elif [ $totalmem -lt 2500000 ] ;
+elif [ "$totalmem" -lt 2500000 ] ;
 then
     MEMORY_BLOCK=256
 else
     MEMORY_BLOCK=1024
 fi
 
-sudo sed -i 's/) * 1024 )/) * ${MEMORY_BLOCK} )/g' /usr/bin/init-zram-swapping # change the default size of zram to 512MB
-
-#TODO: check system cpu and determine if use zstd or lzo as compression system
+sed -i "s/1024)/$MEMORY_BLOCK)/g" /usr/bin/init-zram-swapping 
 
 # set compress algorithm to zstd before the disksize is set
-sudo sed '/^echo zstd > /sys/block/zram${DEVNUMBER}/comp_algorithm.*/i echo $mem' /usr/bin/init-zram-swapping
+sed -i '/echo $mem >/iecho zstd /sys/block/zram${DEVNUMBER}/comp_algorithm' /usr/bin/init-zram-swapping
+OPTIONS=("vm.vfs_cache_pressure=500" "vm.swappiness=100" "vm.dirty_background_ratio=1" "vm.dirty_ratio=50" "vm.page-cluster=0")
 
-OPTIONS = ("vm.vfs_cache_pressure=500" "vm.swappiness=100" "vm.dirty_background_ratio=1" "vm.dirty_ratio=50" "vm.page-cluster=0")
-
-for s in ${OPTIONS[@]} ; do
-    sudo echo $s >> /etc/sysctl.conf
+for s in "${OPTIONS[@]}" ; do
+    printf "%s\n" "${s}" >> /etc/sysctl.conf
 done 
 
 notify-send 'zram impostata!'
