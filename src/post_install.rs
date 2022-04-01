c7321a7a1e24229fb51f1c21e52d9b9ac70c993f
crossedheadx <cesarevigliani89@gmail.com>
2022-04-01 23:54:30 +0200

    WIP
    feature: add enable_canonical_partners fn
    fix: turn post_install_steps async.

--- src/post_install.rs
+++ src/post_install.rs
@@ -1,22 +1,43 @@
-pub mod Install {
+pub mod install {
     use cmd_lib::{run_cmd, CmdResult};
     use crate::messages::out::*;
     use crate::op_types::{PostInstallMode};
 
-    pub fn post_install_steps(mode: PostInstallMode) -> CmdResult {
-        loading();
 
-        // enable_repos()
+    pub async fn post_install_steps(mode: PostInstallMode) -> CmdResult {
+        loading().await;
+
+        enable_repos()?;
+        enable_canonical_partners()?;
+        do_updates(Some(true))?;
+
         Ok(())
     }
 
     fn enable_repos() -> CmdResult {
         let v: Vec<&str> = vec!["universe", "multiverse", "restricted"];  
         v.into_iter().for_each(|i:&str| {
-            let cmd = format!("add-apt-repository -y {}", i);
-            run_cmd!{ cmd };
+            let cmd: String = format!("add-apt-repository -y {}", i);
+            run_cmd!( $cmd ).unwrap();
         });
         
+        Ok(())
+    }
+
+    fn enable_canonical_partners()-> CmdResult {
+        let cmd1: String = format!("deb http://archive.canonical.com/ubuntu $(lsb_release -cs) partner");
+        let cmd2: String = format!("deb-src http://archive.canonical.com/ubuntu $(lsb_release -cs) partner" );
+
+        run_cmd!( echo $cmd1 | sudo tee -a /etc/apt/sources.list ).unwrap();
+        run_cmd!( echo $cmd2 | sudo tee -a /etc/apt/sources.list )
+    }
+
+    pub fn do_updates(snap: Option<bool>) -> CmdResult {
+        if snap.unwrap_or(true) {
+            run_cmd!("sudo apt update && sudo apt -y full-upgrade && sudo snap refresh").unwrap();
+        } else {
+            run_cmd!("sudo apt update && sudo apt -y full-upgrade").unwrap();
+        }
         Ok(())
     }
 }
