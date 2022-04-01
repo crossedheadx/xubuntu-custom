e6fc5fffb47761f4cd28a9bcd7507dd7cfa139fc
crossedheadx <cesarevigliani89@gmail.com>
2022-04-01 23:53:15 +0200

    WIP turn fn async.

--- src/operatives.rs
+++ src/operatives.rs
@@ -1,37 +1,37 @@
 
 pub mod op {
+    use std::future::Future;
+
     use cmd_lib::CmdResult;
     use crate::messages::out::*;
-    use crate::utilities::utility::{read};
-    use crate::post_install::Install::post_install_steps;
-    use crate::op_types::{PostInstallMode};
-  
-    pub fn startup() -> CmdResult {
+    use crate::utilities::utility::read;
+    use crate::{post_install::install::post_install_steps, op_types::PostInstallMode};
+      
+    pub async fn startup() -> Box<dyn Future<Output = CmdResult>> {
         print_menu();
         let user_input:String = read();
         match user_input.as_str() {
-            "s\n" => post_install_steps(PostInstallMode::Automatic),
-            "n\n" => post_install_steps(PostInstallMode::Manual),
+            "s\n" => Box::new(post_install_steps(PostInstallMode::Automatic)),
+            "n\n" => Box::new(post_install_steps(PostInstallMode::Manual)),
             "i\n" => {
                 print_infos(); 
-                clean_screen(); 
-                return startup();
+                clean_screen().unwrap(); 
+                return startup().await;
             },
-            _ => stop()
-        };
-        Ok(())
+            _ => return stop()
+        }
     }
     
-    fn stop() -> CmdResult {
+    async fn stop() -> CmdResult {
         println!("vuoi interrompere? [s]ì [n]o");
         let user_input:String = read();
         match user_input.as_str() {
             "s\n" => return Ok(()),
             "n\n" => { 
-                clean_screen(); 
-                return startup()
+                clean_screen().unwrap(); 
+                return startup().await
             },
-            _ => return stop()
+            _ => return stop().await
         }
     }
 }
