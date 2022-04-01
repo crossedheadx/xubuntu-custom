0e10e005098e3b0691882d2e326983f87b0ed01b
Cesare Vigliani <cesare.vigliani@elvispos.com>
2022-04-01 17:21:07 +0200

    Add downloader
    feature: add two new packages to Cargo.toml
    feature: add download function.

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,6 +1,8 @@
 pub mod utility {
     use std::{io, thread, time};
-
+    use std::io::Cursor;
+    type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+ 
     pub fn read() -> String {
         let mut user_input:String = String::new();
         io::stdin().read_line(&mut user_input).expect("Failed to read input");
@@ -13,6 +15,15 @@ pub mod utility {
 
     pub fn sleep(seconds: u64) {
         thread::sleep(time::Duration::from_millis(seconds));
+    }
+
+    // downloader 
+    async fn donwload(url: String, file_name: String) -> Result<()> {
+        let response = reqwest::get(url).await?;
+        let mut file = std::fs::File::create(file_name)?;
+        let mut content =  Cursor::new(response.bytes().await?);
+        std::io::copy(&mut content, &mut file)?;
+        Ok(())
     }
 }
 
