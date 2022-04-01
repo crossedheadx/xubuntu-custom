3a975b8618567ac29efb331777aa3d553e5d9c2d
crossedheadx <cesarevigliani89@gmail.com>
2022-04-01 23:54:48 +0200

    WIP start implementing get_packages.

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,7 +1,8 @@
 pub mod utility {
-    use std::{io, thread, time};
+    use std::{io, thread, time, fs};
     use std::io::Cursor;
     type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+    use crate::op_types::{ListCategories};
  
     pub fn read() -> String {
         let mut user_input:String = String::new();
@@ -24,6 +25,14 @@ pub mod utility {
         let mut content =  Cursor::new(response.bytes().await?);
         std::io::copy(&mut content, &mut file)?;
         Ok(())
+    }
+
+    fn get_packages() -> ListCategories {
+        let path: &str = "./packages.json";
+        let data:String = fs::read_to_string(path).expect("Error reading file");
+        let res: serde_json::Value = serde_json::from_str(&data).expect("Error parsing json");
+        println!("{:?}", res);
+        // Todo: parse into obj with given struct
     }
 }
 
